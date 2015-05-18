6347bb51a57138035eec9a0e810abc53f75674c3
Spencer Mathews <samathews@LYNX>
2015-05-18 15:20:15 -0700

    Improve package script and commit first package list, from LYNX

--- scripts/Rpackages.R
+++ scripts/Rpackages.R
@@ -4,18 +4,15 @@
 # I grabbed useful code from a post and created functions
 #
 
-save_package_list <- function() {
+
+save_package_list <- function(file_name = "Rpackages.Rdata") {
   packages <- installed.packages()[,"Package"]
-  save(packages, file="Rpackages")
+  save(packages, file=file_name)
+  return(packages)
 }
 
-install_from_package_list <- function() {
-  load("Rpackages")
+install_from_package_list <- function(file_name = "Rpackages.Rdata") {
+  load(file_name)
   for (p in setdiff(packages, installed.packages()[,"Package"]))  install.packages(p)
 }
-
-
-
-
-
 
